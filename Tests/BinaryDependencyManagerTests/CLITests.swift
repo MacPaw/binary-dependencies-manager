7c97bc7889f8a986b8806c6696ef0493224f00c8
Vitalii Budnik <nekrich@users.noreply.github.com>
2025-06-20 17:26:11 +0300

    chore: chore: add CLI and GitHub (#12)

---
+++ Tests/BinaryDependencyManagerTests/CLITests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import binary_dependencies_manager
+
+final class CLITests: XCTestCase {
+    func testWhichFindsExistingTool() throws {
+        let url = try CLI.which(cliToolName: "ls")
+        XCTAssertTrue(url.path.hasSuffix("/ls"), "Should find 'ls' command")
+    }
+
+    func testWhichThrowsForNonexistentTool() {
+        XCTAssertThrowsError(try CLI.which(cliToolName: "nonexistent_tool_12345")) { error in
+            let nsError = error as NSError
+            XCTAssertTrue(nsError.domain.contains("nonexistent_tool_12345"))
+        }
+    }
+}
