91174c7b7ea6c2a6cd2349b4255dcd37bbb64428
Vitalii Budnik <nekrich@users.noreply.github.com>
2025-06-23 13:40:18 +0300

    chore: refactor resolve command

--- Sources/CommandLine/Commands/ResolveCommand.swift
+++ Sources/CommandLine/Commands/ResolveCommand.swift
@@ -1,27 +1,48 @@
 import ArgumentParser
 import BinaryDependencyManager
+import Foundation
+import Yams
+import Utils
 
 struct ResolveCommand: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "resolve",
+        version: BinaryDependenciesManager.configuration.version
+    )
 
-    static var configuration = CommandConfiguration(commandName: "resolve")
+    /// Path to the configuration file.
+    @Option(name: .shortAndLong, help: "Path to the configuration file")
+    var configurationFilePath: String?
 
-    // Path to dependencies json
-    @Option(name: .shortAndLong, help: "Path to dependencies json")
-    var dependenciesJSONPath: String
+    /// Path to the cache directory.
+    @Option(name: .long, help: "Path to the cache directory")
+    var cacheDirectoryPath: String?
 
-    // Path to cache directory
-    @Option(name: .shortAndLong, help: "Path to cache directory")
-    var cacheDirectoryPath: String
+    /// Path to the output directory.
+    @Option(name: .long, help: "Path to the output directory, where downloaded dependencies will be placed")
+    var outputDirectoryPath: String?
 
-    // Path to output directory
-    @Option(name: .shortAndLong, help: "Path to output directory, where downloaded dependencies will be placed")
-    var outputDirectoryPath: String
+    /// Dependencies to resolve.
+    var configuration: BinaryDependenciesConfiguration?
+
+    /// Validates a given configuration file.
+    mutating func validate() throws {
+        let configurationReader: BinaryDependenciesConfigurationReader = .init()
+        configuration = try configurationReader
+            .readConfiguration(at: configurationFilePath, currentToolVersion: BinaryDependenciesManager.version)
+        outputDirectoryPath = configurationReader
+            .resolveOutputDirectoryURL(outputDirectoryPath ?? configuration?.outputDirectory)
+            .path
+        cacheDirectoryPath = configurationReader
+            .resolveCacheDirectoryURL(cacheDirectoryPath ?? configuration?.cacheDirectory)
+            .path
+    }
 
     func run() throws {
         let resolver = DependenciesResolverRunner(
-            dependenciesJSONPath: dependenciesJSONPath,
-            cacheDirectoryPath: cacheDirectoryPath,
-            outputDirectoryPath: outputDirectoryPath
+            dependencies: configuration!.dependencies,
+            cacheDirectoryPath: cacheDirectoryPath!,
+            outputDirectoryPath: outputDirectoryPath!
         )
         try resolver.run()
     }
