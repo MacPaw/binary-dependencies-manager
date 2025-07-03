c2dfc5004fe2f816b703ab5f8c5912b1410193ce
Vitalii Budnik <nekrich@users.noreply.github.com>
2025-07-03 15:24:42 +0300

    chore(build): bump swift-tools-version to 6.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -62,7 +62,7 @@ let package = Package(
         ),
     ],
 
-    swiftLanguageVersions: [
-        .version("6")
+    swiftLanguageModes: [
+        .v6
     ]
 )
