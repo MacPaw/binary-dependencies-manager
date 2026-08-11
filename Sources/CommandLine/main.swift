07975f71d42c1bb94561d5d079024ba8cf84296d
Vitalii Budnik <nekrich@users.noreply.github.com>
2026-08-11 10:17:44 +0300

    chore: bump version (#74)

--- Sources/CommandLine/main.swift
+++ Sources/CommandLine/main.swift
@@ -17,7 +17,7 @@ setbuf(stdout, nil)
 
 struct BinaryDependenciesManager: ArgumentParser.ParsableCommand {
     /// The version of the binary dependencies manager.
-    static let version: Version = "0.1.0"
+    static let version: Version = "0.1.1"
 
     static let configuration = CommandConfiguration(
         abstract: "Binary dependencies resolver",
